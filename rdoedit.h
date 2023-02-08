ea90b749a3352292d7e2788fe9156648663fa12c
stefan11111 <stefanalexe48@gmail.com>
2023-02-08 22:47:18 +0200

    require password by default

--- rdoedit.h
+++ rdoedit.h
@@ -13,7 +13,7 @@
 #define ALLOWED_USER "stefan"		/*the allowed user*/
 #define FILENAME "aaaabbbbccccdddd"	/*temporary filename*/
 #define EDITOR "/bin/nano"		/*fallback editor*/
-#define REQUIRE_PASSWORD 0		/*require password for the allowed user*/
+#define REQUIRE_PASSWORD 1		/*require password for the allowed user*/
 					/*1 is true, 0 is false*/
 #define ALLOW_ROOT 1			/*allow the root user to use rdo*/
 					/*1 is true, 0 is false*/
